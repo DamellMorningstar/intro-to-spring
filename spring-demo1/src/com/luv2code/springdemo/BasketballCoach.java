a4459b9fa510a87d0b65d30e8ebcc08f4f002056
Da-Mell <Damellfaines8@gmail.com>
2021-07-15 13:59:07 -0400

    almost finished

---
+++ spring-demo1/src/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return " Spend 30 minutes shooting free throws";
+	}
+}
---
+++ spring-demo1/src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class SoccerCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Pratice dribbling for an hour";
+	}
+
+}
